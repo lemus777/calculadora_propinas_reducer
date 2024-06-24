cd072481c3ef72fac437eebfb3c2e530edb80f8d
lemus <lemus777@gmail.com>
2024-06-24 13:03:58 +0200

    reducer and actions created

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
+import { useReducer } from "react"
 import MenuItem from "./components/MenuItem"
 import OrderContents from "./components/OrderContents"
 import OrderTotals from "./components/OrderTotals"
 import TipPercentageForm from "./components/TipPercentageForm"
 import { menuItems } from "./data/db"
 import useOrder from "./hooks/useOrder"
+import { initialState, orderReducer } from "./reducers/order-reducer"
 
 function App() {
 
   const { order, tip, setTip, addItem, removeItem, placeOrder } = useOrder()
+  const [state, dispatch] = useReducer(orderReducer, initialState)
   
   return (
     <>
